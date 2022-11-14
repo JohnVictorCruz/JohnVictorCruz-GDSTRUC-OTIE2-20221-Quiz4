4dd18e732a11e19d5f5baff9ffea0356fc4978f9
John Victor Cruz <johnvictor.cruz@benilde.edu.ph>
2022-11-14 10:41:24 +0800

    Added finished Quiz 4

---
+++ GDSTRUC-OTIE2-20221-Quiz4/src/com/gdstruc/otie2/module5/Main.java
@@ -0,0 +1,30 @@
+package com.gdstruc.otie2.module5;
+
+public class Main {
+    public static void main(String[] args) {
+        Player pikachu = new Player(25, "Pikachu", 1);
+        Player celebi = new Player(251, "Celebi", 2);
+        Player hitmonchan = new Player(107, "Hitmonchan", 3);
+        Player mankey = new Player(56, "Mankey", 4);
+        Player starmie = new Player(175, "Starmie", 4);
+
+        SimpleHashtable hashtable = new SimpleHashtable();
+
+        // add initial players
+        hashtable.put(celebi.getUserName(), celebi);
+        hashtable.put(mankey.getUserName(), mankey);
+        hashtable.put(pikachu.getUserName(), pikachu);
+        hashtable.put(hitmonchan.getUserName(), hitmonchan);
+        hashtable.put(starmie.getUserName(), starmie);
+
+        //Remove function
+        hashtable.remove("");
+
+
+        // print the contents of the hashtable
+        hashtable.printHashtable();
+
+        // get a value based on a given key
+        //System.out.println(hashtable.get("Mankey"));
+    }
+}
